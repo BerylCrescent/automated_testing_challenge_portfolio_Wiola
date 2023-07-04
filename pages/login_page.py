07e9fabd519c1efef2ee3c56d7edceeb85a69b44
Wioletta Rusiecka <crescent.beryl@gmail.com>
2023-07-04 12:53:26 +0200

    selectors_task

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,10 +1,13 @@
-# from pages.base_page import BasePage
-#
-#
-# class LoginPage(BasePage):
-#     login_field_xpath = "//*[@id='login']"
-#     password_field_xpath =
-#     sign_in_button_xpath =
-#
-#     def type_in_email(self, email):
-#         self.field_send_keys(self.login_field_xpath, email)
+from pages.base_page import BasePage
+
+
+class LoginPage(BasePage):
+    login_field_xpath = "//*[@id='login']"
+    password_field_xpath = "//*[@id='password']"
+    sign_in_button_xpath = "//button[@type='submit']"
+    language_listbox_xpath = "//form/div/div[2]/div/div"
+    remind_password_hyperlink_xpath = "//a[text()='Remind password']"
+    scouts_panel_text_xpath = "//form/div/div[1]/h5"
+
+    def type_in_email(self, email):
+        self.field_send_keys(self.login_field_xpath, email)
